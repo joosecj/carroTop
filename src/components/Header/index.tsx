67974859e80f34e7386c740489d6fb1c85c61978
Jose Carlos <josecarloscjj@gmail.com>
2022-12-04 18:34:47 -0300

    feat: clear  project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header className="ct-header">
+      <nav className="ct-container">
+        <h1>Carro Top</h1>
+      </nav>
+    </header>
+  );
+}
